69c793005ab318fac715e635a84033867dca487d
anandfingertip <anand@fingertip.org>
2016-08-10 14:00:33 +0300

    Update DemoComponentController.js

--- src/aura/DemoComponent/DemoComponentController.js
+++ src/aura/DemoComponent/DemoComponentController.js
@@ -3,7 +3,7 @@
 		component.set("v.output", "5 + 5 = " + window.FT.add(5,5));
 	},
 	execute2: function(component) {
-		var testFunction = Function('a', 'b', 'window.FT.add(5,5)');
+		var testFunction = Function('a', 'b', 'window.FT.add(a,b)');
 
 		component.set("v.output", "5 + 5 = " + testFunction(10,10));
 	}
